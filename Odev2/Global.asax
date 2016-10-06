c8317a8463e55c2acf3d931f3a6172693b485ef9
berkaltin <beko3517@hotmail.com>
2016-10-06 15:36:24 +0300

    Add files via upload

---
+++ Odev2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BerkWeb.MvcApplication" Language="C#" %>
---
+++ Odev2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BerkWeb.Startup))]
+namespace BerkWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
